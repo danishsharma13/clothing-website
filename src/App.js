d69b5c0455ccda0c8fb5fce4204eebd035e3c53d
Danish Sharma <danishsharma1999@gmail.com>
2020-10-22 22:21:02 -0400

    updating shop page, preview and collection

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from 'react';
-import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage';
-
-const HatsPage = () => {
-  return (<div>
-    <h1>Hats Page</h1>
-  </div>)
-}
+import ShopPage from './pages/shop/shop';
+import './App.css';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/hats' component={HatsPage} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/shop/shop.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import ShopData from './shop.data';
+import PreviewCollection from '../../components/preview-collection/preview-collection';
+
+class ShopPage extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state= {
+            collections: ShopData
+        }  
+    }
+
+    render() {
+        const {collections} = this.state;
+        return(
+            <div className='shop-page'>
+               {
+                   collections.map(({id, ...otherCollectionProps}) => (
+                       <PreviewCollection key={id} {...otherCollectionProps} />
+                   ))
+               }
+            </div>
+        )
+    }
+}
+
+export default ShopPage;
